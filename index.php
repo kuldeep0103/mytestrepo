975e2dbe11098fc818510dd7a075c7ab2f7ef35f
Kuldeep Singh <kuldeep.developer91@gmail.com>
2022-08-13 14:00:00 +0530

    second commit

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <p align="center">*********************************************************************************************************************************************/<br>
 <br>
-Coverage 28East By WHMCS Services
+ 
  <br>
-Copyright WHMCS Services, All Rights Reserved<br>
-Created By WHMCSServices <a href="http://www.whmcsservices.com">http://www.whmcsservices.com</a>
+only for testing git
 <br>
 <p align="center">
 ********************************************************************************************************************************************/</p>
